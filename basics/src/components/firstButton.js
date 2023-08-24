ee89ff87fdcab442e83babe6c87b917ba24df974
DKReyes <dylankreyes@gmail.com>
2023-08-24 13:26:22 -0500

    Added more information on useEffect and props

--- basics/src/components/firstButton.js
+++ basics/src/components/firstButton.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // This is the outcome of what is displayed
 function Display() {
@@ -9,10 +9,42 @@ function Display() {
     )
 }
 
+// Props
+function Dog(props) {
+    return <p>I'm a dog and my name is {props.name}.</p>
+}
+
 
 function FirstButton() {
-    // a boolean state
+    // a boolean state (for useState example)
     const [answer, setAnswer] = useState(false);
+    // a count state (for useEffect example)
+    const [count, setCount] = useState(0);
+    // a prop element (for props example)
+    const theDog = <Dog name="Danny"/>;
+
+
+    // useEffect will run on every render, so the count changes every 1000 tick set by the 
+    // parameters on setTimeout. The [] on useEffect will only run this on inital render
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count + 1);
+        }, 1000);
+    }, []);
+
+    // Example of useEffect running only once
+    /* Runs on the first render
+     * useEffect(() => {
+     * 
+     * }, []);
+     */
+
+    // Example of useEffect running once a prop or state values
+    /* Runs on the first render and any time any dependency value changes eg. prop or state
+     * useEffect(() => {
+     *
+     * }, [prop, state]);
+     */
 
 
     // Handles the onClick event
@@ -28,6 +60,9 @@ function FirstButton() {
             </button>
 
             {answer && <Display />}
+
+            <p>Website has been rendered {count} times.</p>
+            <p>{theDog}</p>
         </>
     );
 }
